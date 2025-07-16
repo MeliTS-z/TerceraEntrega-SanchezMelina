bee87a02fe8f7bd44f7471d5c65dbe6c2ca9cb0a
MeliTS-z <melitsanchez@gmail.com>
2025-07-16 00:13:56 -0300

    Prueba save amigo

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 #include
-from .views import landing
+from .views import landing, crear_amigo
 
 urlpatterns = [
     path('myapp/landing', landing),
+    path('myapp/crear-amigo/<str:nombre>/', crear_amigo),
 ]
 
 #path('admin/', admin.site.urls),
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import Amigo
 from django.http import HttpResponse
 
 # Create your views here.
 def landing(request):
     return render(request, "myapp/landing.html")
 
+def crear_amigo(request, nombre):
+    if nombre is not None:
+        nuevo_amigo = Amigo(
+            nombre=nombre,
+            fecha_nacimiento_estimada="2024-12-12",
+            especie="Mestizo",
+            descripcion="Enérgico"
+        )
+        nuevo_amigo.save()
 
+    return render(request, "myapp/crear_amigo.html", {"nombre":nombre})    
 
